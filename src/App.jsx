b2c1ddd116e2b6e846db86bd01a2d45663e91c2b
Mannelito-1 <Armando12125@hotmail.com>
2025-10-25 12:16:57 +0200

    Add LogosSection component and Logoipsum 5 SVG image
    - Created a new LogosSection component to display partner logos.
    - Added Logoipsum 5 SVG image to the project.
    - Updated the LogosSection to include multiple logo images.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import AboutUs from './components/AboutUs'
+import LogosSection from './components/LogosSection'
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
     <Hero />
 
     <AboutUs/>
+
+    <LogosSection/>
     
     
     
---
+++ src/components/LogosSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function LogosSection() {
+  return (
+    <>
+    <section class="partners">
+      <div class="container-space">
+        <div class="partners-logo">
+          <div class="logo">
+            <img src="src/components/Images/Logoipsum 1.svg" alt="" />
+          </div>
+          <div class="logo">
+            <img src="src/components/Images/Logoipsum 2.svg" alt="" />
+          </div>
+          <div class="logo">
+            <img src="src/components/Images/Logoipsum 3.svg" alt="" />
+          </div>
+          <div class="logo">
+            <img src="src/components/Images/Logoipsum 4.svg" alt="" />
+          </div>
+          <div class="logo">
+            <img src="src/components/Images/Logoipsum 5.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    
+    
+    
+    </>
+  )
+}
+
+export default LogosSection
