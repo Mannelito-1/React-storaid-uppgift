45208aeee51f98de6a0be3cb29b9bc2d51725fc2
Mannelito-1 <Armando12125@hotmail.com>
2025-11-08 14:52:17 +0100

    added hover effect to each pricing card, added correct numbers and fixed styling issues

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Titlecomp from "./Titlecomp";
 import Btn from "./Btn";
 
 function Pricing() {
+  const [onHover, setOnHover] = useState(null);
   return (
     <>
       <section className="pricing">
@@ -11,138 +12,174 @@ function Pricing() {
           h3Title="Find the Perfect Plan for Your Storage Needs"
         />
         <div className="container-space">
-            <div className="pricingCard">
-                <h5>Small Unit</h5>
-                <div className="priceNumber">
-                    <h4>$</h4>
-                    <h2>50</h2>
-                    <h5>/month</h5>
-                </div>
-                <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
-                <p className="line">──────────────────────</p>
-                <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                <div className="pricing-btn">
-                    <Btn btnText="Rent now"/>
-                </div>
+          <div
+            className={`pricingCard ${
+              onHover === "small unit" ? "active" : ""
+            }`}
+            onMouseEnter={() => setOnHover("small unit")}
+            onMouseLeave={() => setOnHover(null)}
+          >
+            <h5>Small Unit</h5>
+            <div className="priceNumber">
+              <h4 className="dollar">$</h4>
+              <h2>50</h2>
+              <h5 className="month">/month</h5>
             </div>
-            <div className="pricingCard">
-                <h5>Small Unit</h5>
-                <div className="priceNumber">
-                    <h4>$</h4>
-                    <h2>50</h2>
-                    <h5>/month</h5>
-                </div>
-                <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
-                <p className="line">────────────────────────────</p>
-                <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                <div className="pricing-btn">
-                    <Btn btnText="Rent now"/>
-                </div>
+            <p className="p-text">
+              Phasellus sollicitudin sapien eu massa accumsan, quis semper odio
+              suscipit.
+            </p>
+            <p className="line">──────────────────────</p>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
             </div>
-            <div className="pricingCard">
-                <h5>Small Unit</h5>
-                <div className="priceNumber">
-                    <h4>$</h4>
-                    <h2>50</h2>
-                    <h5>/month</h5>
-                </div>
-                <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
-                <p className="line">────────────────────────────</p>
-                <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                <div className="pricing-btn">
-                    <Btn btnText="Rent now"/>
-                </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
             </div>
-            <div className="pricingCard">
-                <h5>Small Unit</h5>
-                <div className="priceNumber">
-                    <h4>$</h4>
-                    <h2>50</h2>
-                    <h5>/month</h5>
-                </div>
-                <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
-                <p className="line">────────────────────────────</p>
-                <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                   <div className="priceP">
-                    <p className="checkmark">✔</p>
-                    <p>Nam nec ipsum in dolor</p>
-                </div>
-                <div className="pricing-btn">
-                    <Btn btnText="Rent now"/>
-                </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
             </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="pricing-btn">
+              <Btn btnText="Rent now" />
+            </div>
+          </div>
+          <div
+            className={`pricingCard ${
+              onHover === "medium unit" ? "active" : ""
+            }`}
+            onMouseEnter={() => setOnHover("medium unit")}
+            onMouseLeave={() => setOnHover(null)}
+          >
+            <h5>Medium Unit</h5>
+            <div className="priceNumber">
+              <h4 className="dollar">$</h4>
+              <h2>100</h2>
+              <h5 className="month">/month</h5>
+            </div>
+            <p className="p-text">
+              Phasellus sollicitudin sapien eu massa accumsan, quis semper odio
+              suscipit.
+            </p>
+            <p className="line">──────────────────────</p>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="pricing-btn">
+              <Btn btnText="Rent now" />
+            </div>
+          </div>
+          <div
+            className={`pricingCard ${
+              onHover === "large unit" ? "active" : ""
+            }`}
+            onMouseEnter={() => setOnHover("large unit")}
+            onMouseLeave={() => setOnHover(null)}
+          >
+            <h5>Large Unit</h5>
+            <div className="priceNumber">
+              <h4 className="dollar">$</h4>
+              <h2>150</h2>
+              <h5 className="month">/month</h5>
+            </div>
+            <p className="p-text">
+              Phasellus sollicitudin sapien eu massa accumsan, quis semper odio
+              suscipit.
+            </p>
+            <p className="line">──────────────────────</p>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="pricing-btn">
+              <Btn btnText="Rent now" />
+            </div>
+          </div>
+          <div
+            className={`pricingCard ${
+              onHover === "executive unit" ? "active" : ""
+            }`}
+            onMouseEnter={() => setOnHover("executive unit")}
+            onMouseLeave={() => setOnHover(null)}
+          >
+            <h5>Executive Unit</h5>
+            <div className="priceNumber">
+              <h4 className="dollar">$</h4>
+              <h2>200</h2>
+              <h5 className="month">/month</h5>
+            </div>
+            <p className="p-text">
+              Phasellus sollicitudin sapien eu massa accumsan, quis semper odio
+              suscipit.
+            </p>
+            <p className="line">──────────────────────</p>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="priceP">
+              <p className="checkmark">✔</p>
+              <p>Nam nec ipsum in dolor</p>
+            </div>
+            <div className="pricing-btn">
+              <Btn btnText="Rent now" />
+            </div>
+          </div>
         </div>
       </section>
     </>
