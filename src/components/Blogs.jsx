a110634e621199f2e75dad6b03cf83eb30305b5a
Mannelito-1 <Armando12125@hotmail.com>
2025-11-10 21:57:45 +0100

    rebuilt blogs and added api to blogs and index so each indivudiual card gets diffrent color when hoverd, added img aswell.

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,106 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Titlecomp from "./Titlecomp";
+import { Link } from "react-router-dom";
 
 function Blogs() {
+  const [onHover, setOnHover] = useState(null);
+
+  const [blogItems, setBlogItems] = useState([]);
+
+  const fetchData = async () => {
+    const res = await fetch(
+      "https://win25-jsf-assignment.azurewebsites.net/api/blogs"
+    );
+    const data = await res.json();
+    setBlogItems(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <section className="blogs">
         <div className="container-space">
-            <div className="title">
-                <Titlecomp
-                    pText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida."
-                    h4Title="Latest Blog and News"
-                    h3Title="Check Out Our Latest Blog and News Update"
-                  />
-            </div>
+          <div className="title">
+            <Titlecomp
+              pText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida."
+              h4Title="Latest Blog and News"
+              h3Title="Check Out Our Latest Blog and News Update"
+            />
+          </div>
 
-            <div className="blogs-div">
-                <div className="blog-card">
-                    <div className="img-container">
-                        <img src="src/components/Images/blogcard-img.svg" alt="" />
-                    </div>
+          <div className="blogs-div">
+            {blogItems.map((item) => (
+              <Link to="/404" className="linkStyle" key={item.id}>
+                <div
+                  className={`blog-card ${onHover === item.id ? "active" : ""}`}
+                  onMouseEnter={() => setOnHover(item.id)}
+                  onMouseLeave={() => setOnHover(null)}
+                  key={item.id}
+                >
+                  <div className="img-container">
+                    <img src={item.imageUrl} />
+                  </div>
 
-                    <div className="img-text">
-                        <img src="src/components/Images/calender-icon.svg" alt="" />
-                        <p>August 17, 2025</p>
-                    </div>
-                    
+                  <div className="img-text">
+                    <img
+                      className="calander-dark"
+                      src="src/components/Images/calender-icon-dark.svg"
+                      alt=""
+                    />
+                    <img
+                      className="calender-light"
+                      src="src/components/Images/calender-icon.svg"
+                      alt=""
+                    />
+                    <p>{new Date(item.created).toLocaleString("en-US", { month: "long", day: "2-digit", year:"numeric"})}</p>
+                  </div>
+                  {/* took help from chatgpt to convert item.created to display month day and year instead of just numbers. */}
 
-                    <div className="blog-card-title">
-                      <h6 className="card-title">Safe and Secure: The Importance of Choosing the Right Storage</h6>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem…</p>
-                    </div>
+                  <div className="blog-card-title">
+                    <h6 className="card-title">{item.title}</h6>
+                    <p className="description">{item.description}</p>
+                  </div>
 
-
-                    <div className="read-more">
-                      <a className="read-more-btn" href="">Read more →</a>
-                    </div>
+                  <div className="read-more">
+                    <p className="read-more-btn">Read more →</p>
+                  </div>
                 </div>
-
-
-
-
-                 <div className="blog-card">
-                    <div className="img-container">
-                        <img src="src/components/Images/blogcard-img.svg" alt="" />
-                    </div>
-
-                    <div className="img-text">
-                        <img src="src/components/Images/calender-icon.svg" alt="" />
-                        <p>August 17, 2025</p>
-                    </div>
-                    
-
-                    <div className="blog-card-title">
-                      <h6 className="card-title">Safe and Secure: The Importance of Choosing the Right Storage</h6>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem…</p>
-                    </div>
-
-
-                    <div className="read-more">
-                      <a className="read-more-btn" href="">Read more →</a>
-                    </div>
-                </div>
-
-
-
-
-
-
-
-
-
-                 <div className="blog-card">
-                    <div className="img-container">
-                        <img src="src/components/Images/blogcard-img.svg" alt="" />
-                    </div>
-
-                    <div className="img-text">
-                        <img src="src/components/Images/calender-icon.svg" alt="" />
-                        <p>August 17, 2025</p>
-                    </div>
-                    
-
-                    <div className="blog-card-title">
-                      <h6 className="card-title">Safe and Secure: The Importance of Choosing the Right Storage</h6>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem…</p>
-                    </div>
-
-
-                    <div className="read-more">
-                      <a className="read-more-btn" href="">Read more →</a>
-                    </div>
-                </div>
-
-
-
-
-              
-                
-               
-            </div>
-
-
+              </Link>
+            ))}
+          </div>
         </div>
       </section>
     </>
