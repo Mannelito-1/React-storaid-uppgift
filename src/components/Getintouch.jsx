eaa4395dd7ab479041030bf917ceb9ca04c3267e
Mannelito-1 <Armando12125@hotmail.com>
2025-11-16 10:49:30 +0100

    made code look better

--- src/components/Getintouch.jsx
+++ src/components/Getintouch.jsx
@@ -12,129 +12,100 @@ function Getintouch() {
   });
 
   const [errors, setErrors] = useState({});
-  const [validSubmit,setValidSubmit] = useState(false)
-
+  const [validSubmit, setValidSubmit] = useState(false);
 
   const validateField = (name, value) => {
-    let error= ''
+    let error = "";
 
-    if (name === 'name' && !/^[A-Za-zÅÄÖåäö\s]{2,}$/.test(value) ) {
-      error = "Must contain at least 2 characters, no numbers"
-    }
-    else if (name === 'email' && !/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(value)) { 
-      error ="Must be a valid email (eg. name@domain.com)"
-      
-    }
-
-    else if (name === 'subject' && !/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(value)) { 
-      error ="Must contain at least 2 characters"
-      
-    }
-
-    else if (name === 'comments' && !/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(value)) { 
-      error ="Must contain at least 2 characters"
-      
-    }
-
-     else if (name === 'telephone' && !/^\+?[0-9\s-]{1,20}$/.test(value)) { 
-      error ="Must contain at numbers"
-      
+    if (name === "name" && !/^[A-Za-zÅÄÖåäö\s]{2,}$/.test(value)) {
+      error = "Must contain at least 2 characters, no numbers";
+    } else if (
+      name === "email" &&
+      !/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(value)
+    ) {
+      error = "Must be a valid email (eg. name@domain.com)";
+    } else if (
+      name === "subject" &&
+      !/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(value)
+    ) {
+      error = "Must contain at least 2 characters";
+    } else if (
+      name === "comments" &&
+      !/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(value)
+    ) {
+      error = "Must contain at least 2 characters";
+    } else if (name === "telephone" && !/^\+?[0-9\s-]{1,20}$/.test(value)) {
+      error = "Must contain at numbers";
     }
 
-
-    
-    setErrors(prev => ({ ...prev, [name]: error }));
-  }
-
-
+    setErrors((prev) => ({ ...prev, [name]: error }));
+  };
 
   const validateForm = () => {
-    const newErrors = {}
+    const newErrors = {};
 
     if (!/^[A-Za-zÅÄÖåäö\s]{2,}$/.test(formData.name))
-      newErrors.name ="Must contain at least 2 characters, no numbers "
+      newErrors.name = "Must contain at least 2 characters, no numbers ";
 
+    if (
+      !/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(formData.email)
+    )
+      newErrors.email = "Must be a valid email (eg. name@domain.com) ";
 
-    if (!/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(formData.email))
-      newErrors.email ="Must be a valid email (eg. name@domain.com) "
+    if (!/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(formData.subject))
+      newErrors.subject = "Must contain at least 2 characters";
 
+    if (!/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(formData.comments))
+      newErrors.comments = "Must contain at least 2 characters";
 
-    if  (!/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(formData.subject))
-      newErrors.subject ="Must contain at least 2 characters"
-
-
-     if  (!/^[A-Za-zÅÄÖåäö0-9\s.,!?'"():;\-]{2,}$/.test(formData.comments))
-      newErrors.comments ="Must contain at least 2 characters"
-
-
-
-    setErrors(newErrors)
-  return Object.keys(newErrors).length === 0
-
-  }
-  
-
-
-
-  
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
 
-
-    validateField(name, value)
+    validateField(name, value);
   };
-
-  
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (validateForm()) {
-      console.log("valid")
-      const  res = await fetch ('https://win25-jsf-assignment.azurewebsites.net/api/contact',  {
-      method:"POST",
-      headers: {
-        'Content-Type' : 'application/json'
-      },
-      body: JSON.stringify({
-        name: formData.name,
-        email: formData.email,
-        phoneNumber: formData.telephone,
-        subject: formData.subject,
-        comment: formData.comments
-      })
-    });
+      console.log("valid");
+      const res = await fetch(
+        "https://win25-jsf-assignment.azurewebsites.net/api/contact",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formData.name,
+            email: formData.email,
+            phoneNumber: formData.telephone,
+            subject: formData.subject,
+            comment: formData.comments,
+          }),
+        }
+      );
 
-    if (res.ok) {
-      setFormData ({
-        name:"",
-        email:"",
-        telephone:"",
-        subject:"",
-        comments:"",
-
-      });
-      setValidSubmit(true);
-      setTimeout(()  => setValidSubmit(false), 3000)
+      if (res.ok) {
+        setFormData({
+          name: "",
+          email: "",
+          telephone: "",
+          subject: "",
+          comments: "",
+        });
+        setValidSubmit(true);
+        setTimeout(() => setValidSubmit(false), 3000);
+      }
+    } else {
+      console.log("not working");
     }
-    }
-
-    else {
-      console.log("not working")
-      
-    }
-
-  
-
-    
-
-  
   };
-
-  
 
   return (
     <>
@@ -156,73 +127,111 @@ function Getintouch() {
           <div className="right-side">
             <form className="form-box" onSubmit={handleSubmit} noValidate>
               <div className="yourname">
-                <p className="form-p">Your name<span className="validation-star"> *</span></p>
+                <p className="form-p">
+                  Your name<span className="validation-star"> *</span>
+                </p>
                 <input
-                  
-                   className={`yourname-input ${errors.name ? "input-error" : ""}`}
+                  className={`yourname-input ${
+                    errors.name ? "input-error" : ""
+                  }`}
                   required
                   type="name"
                   name="name"
                   value={formData.name}
                   onChange={handleChange}
                   placeholder="Your name"
-                /> {errors.name && <span className="validation-error"> {errors.name}  </span>}
+                />{" "}
+                {errors.name && (
+                  <span className="validation-error"> {errors.name} </span>
+                )}
               </div>
 
               <div className="email-tele">
                 <div className="email">
-                  <p className="form-p">Email<span className="validation-star"> *</span></p>
+                  <p className="form-p">
+                    Email<span className="validation-star"> *</span>
+                  </p>
                   <input
-                    className= {`email-input ${errors.email ? "input-error" : ""}`}
+                    className={`email-input ${
+                      errors.email ? "input-error" : ""
+                    }`}
                     type="email"
                     name="email"
                     value={formData.email}
                     onChange={handleChange}
                     required
                     placeholder="Email"
-                  />{errors.email && <span className="validation-error"> {errors.email}  </span>}
+                  />
+                  {errors.email && (
+                    <span className="validation-error"> {errors.email} </span>
+                  )}
                 </div>
 
                 <div className="tele">
                   <p className="form-p">Telephone</p>
                   <input
-                    className= {`tele-input ${errors.telephone ? "input-error" : ""}`}
+                    className={`tele-input ${
+                      errors.telephone ? "input-error" : ""
+                    }`}
                     type="tel"
                     name="telephone"
                     value={formData.telephone}
                     onChange={handleChange}
                     placeholder="Telephone"
-                  />{errors.telephone && <span className="validation-error"> {errors.telephone}  </span>}
+                  />
+                  {errors.telephone && (
+                    <span className="validation-error">
+                      {" "}
+                      {errors.telephone}{" "}
+                    </span>
+                  )}
                 </div>
               </div>
 
               <div className="subject">
-                <p className="form-p">Subjecet<span className="validation-star"> *</span></p>
+                <p className="form-p">
+                  Subjecet<span className="validation-star"> *</span>
+                </p>
                 <input
-                   className= {`subject-input ${errors.subject ? "input-error" : ""}`}
+                  className={`subject-input ${
+                    errors.subject ? "input-error" : ""
+                  }`}
                   type="subject"
                   name="subject"
                   value={formData.subject}
                   onChange={handleChange}
                   required
                   placeholder="How can we help you"
-                />{errors.subject && <span className="validation-error"> {errors.subject}  </span>}
+                />
+                {errors.subject && (
+                  <span className="validation-error"> {errors.subject} </span>
+                )}
               </div>
 
               <div className="comments">
-                <p className="form-p">Comments / Questions<span className="validation-star"> *</span></p>
+                <p className="form-p">
+                  Comments / Questions
+                  <span className="validation-star"> *</span>
+                </p>
                 <textarea
-                   className= {`comments-input ${errors.comments ? "input-error" : ""}`}
+                  className={`comments-input ${
+                    errors.comments ? "input-error" : ""
+                  }`}
                   name="comments"
                   value={formData.comments}
                   onChange={handleChange}
                   required
                   placeholder="Comments"
-                ></textarea>{errors.comments && <span className="validation-error"> {errors.comments}  </span>}
+                ></textarea>
+                {errors.comments && (
+                  <span className="validation-error"> {errors.comments} </span>
+                )}
               </div>
-              {validSubmit && <p className="valid-submit">Thank you for your Question</p>}
+              {validSubmit && (
+                <p className="valid-submit">Thank you for your Question</p>
+              )}
 
-              <Btn className="btn" type="submit" btnText="Submit"/>
+              <Btn className="btn" type="submit" btnText="Submit" />
             </form>
           </div>
         </div>
