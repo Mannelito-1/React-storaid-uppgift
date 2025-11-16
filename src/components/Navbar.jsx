60c9c3bee31d15a03f21b9a0fd62fda3b4a3f663
Mannelito-1 <Armando12125@hotmail.com>
2025-11-16 15:23:50 +0100

    added on foucs and aria labels to what was needed. and on active to change color so its easy to see

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,45 +5,45 @@ import Btn from "./Btn";
 function Navbar() {
   return (
     <>
-      <section className="nav">
+      <nav className="nav" aria-label="Navigation">
         <div className="container">
           <div className="logotype">
-            <Link to="/">
-              <img src="src/components/Images/logo-type.svg" alt="" />
+            <Link to="/" aria-label="Homepage">
+              <img src="src/components/Images/logo-type.svg" alt="brand logo" />
             </Link>
           </div>
 
           <div className="nav-menu">
             <div className="home-btn">
-              <NavLink className="text-lg" to="/">
+              <NavLink  to="/" className={({isActive}) => (isActive ? "text-lg nav-active" : "text-lg")}>
                 Home
               </NavLink>
             </div>
 
             <div className="about-us-btn">
-              <NavLink className="text-lg" to="About">
+              <NavLink  to="/about" className={({isActive}) => (isActive ? "text-lg nav-active" : "text-lg")}>
                 About us
               </NavLink>
             </div>
 
             <div className="services-btn">
-              <NavLink className="text-lg" to="/Services">
+              <NavLink  to="/services" className={({isActive}) => (isActive ? "text-lg nav-active" : "text-lg")}>
                 Services
               </NavLink>
             </div>
 
             <div className="contact-us-btn">
-              <NavLink className="text-lg" to="Contact">
+              <NavLink to="/contact" className={({isActive}) => (isActive ? "text-lg nav-active" : "text-lg")}>
                 Contact us
               </NavLink>
             </div>
           </div>
 
           <div className="book-now">
-            <Btn btnText="Booking"/>
+            <Btn  btnText="Booking" />
           </div>
         </div>
-      </section>
+      </nav>
     </>
   );
 }
